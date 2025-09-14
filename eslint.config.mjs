1769de07006394627bb88bb963ac730eb95b0420
Steven Lin <linjt267@outlook.com>
2025-09-14 17:14:02 +0800

    Update dependencies

---
+++ eslint.config.mjs
@@ -0,0 +1,31 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+
+export default defineConfig([globalIgnores(["**/out", "**/dist", "**/*.d.ts"]), {
+    plugins: {
+        "@typescript-eslint": typescriptEslint,
+    },
+
+    languageOptions: {
+        parser: tsParser,
+        ecmaVersion: 6,
+        sourceType: "module",
+    },
+
+    rules: {
+        "indent": ["warn", 2],
+        "@typescript-eslint/naming-convention": "warn",
+        "quotes": "warn",
+        "semi": "warn",
+        curly: "warn",
+        eqeqeq: "warn",
+        indent: "off",
+        "max-len": ["warn", 120],
+        "no-throw-literal": "warn",
+        "no-trailing-spaces": "warn",
+        "prefer-const": "warn",
+        quotes: "off",
+        semi: "off",
+    },
+}]);
