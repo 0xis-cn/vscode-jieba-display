d95f7ecea41d950d36489bb426804f22ac7a987a
Steven Lin <linjt267@outlook.com>
2025-09-16 23:26:00 +0800

    Fix backward select

--- src/command.ts
+++ src/command.ts
@@ -112,7 +112,7 @@ export function selectWord() {
   });
 }
 
-function searchForward(moveAnchor: boolean = true): {
+function searchForward(moveAnchor = true): {
   newSelections: vscode.Selection[];
   rangesToDelete: vscode.Range[];
 } {
@@ -185,7 +185,7 @@ function searchForward(moveAnchor: boolean = true): {
   return { newSelections, rangesToDelete };
 }
 
-function searchBackward(moveAnchor: boolean = true): {
+function searchBackward(moveAnchor = true, thickCursor = true): {
   newSelections: vscode.Selection[];
   rangesToDelete: vscode.Range[];
 } {
@@ -193,7 +193,15 @@ function searchBackward(moveAnchor: boolean = true): {
   const selections = vscode.window.activeTextEditor!.selections;
 
   const createNewSelection = (oldSelection: vscode.Selection, newActive: vscode.Position) => {
-    return new vscode.Selection(moveAnchor ? newActive : oldSelection.anchor, newActive);
+    if (moveAnchor) {
+      return new vscode.Selection(newActive, newActive);
+    } else if (thickCursor && oldSelection.active > oldSelection.anchor && newActive <= oldSelection.anchor) {
+      // vim visual 模式下刚刚按下v选中单个字符，例如第a行第b个字符，此时 selection=[a:b -> a:b+1)|，光标覆盖该字符
+      // 此时后退选择，需要将 anchor 往右移动一位，以确保 selection 仍覆盖字符 [a:b]，与 vim 原生行为一致
+      return new vscode.Selection(oldSelection.anchor.translate(0, 1), newActive);
+    } else {
+      return new vscode.Selection(oldSelection.anchor, newActive);
+    }
   };
 
   const newSelections: vscode.Selection[] = [];
