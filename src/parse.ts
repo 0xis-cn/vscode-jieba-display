c28bca213ba224482efb657927cf3034e52897c8
Steven Lin <linjt267@outlook.com>
2025-09-16 14:23:37 +0800

    Add cache before tokenize

--- src/parse.ts
+++ src/parse.ts
@@ -1,5 +1,13 @@
-import { tokenize } from "@stevenlin/jieba-wasm";
+import { tokenize, Token } from "@stevenlin/jieba-wasm";
+import QuickLRU from 'quick-lru';
 
-export function parseSentence(sentence: string) {
-  return tokenize(sentence, "default", true);
+const cache = new QuickLRU<String, Token[]>({ maxSize: 25 });
+
+export function parseSentence(sentence: string): Token[] {
+  if (cache.has(sentence)) {
+    return cache.get(sentence)!;
+  }
+  const tokens = tokenize(sentence, "default", true);
+  cache.set(sentence, tokens);
+  return tokens;
 }
--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -19,6 +19,7 @@ suite("Extension Test Suite", () => {
   test("English test", englishTest);
   test("Javascript expression test", jsExpTest);
   test("Selection test", selectionTest);
+  test("Long text test", longTextTest);
 });
 
 const chnText = "“自由软件”尊重用户的自由，并且尊重整个社区。";
@@ -42,6 +43,14 @@ Copyleft
 出口条列
 法律考虑
 基于合同的许可证`;
+
+/* eslint-disable max-len */
+const longText = `“自由地运行程序”（自由度0）意味着任何人或组织可以在任何（支持的）计算机系统上，出于任何目的去运行该程序，并且没有义务通知软件的开发者或任何个人或团体。此处，用户的目的是重点，而非开发者的目的。如果你是该程序的用户，那么你就可以自由地运行该程序。同理，如果你把程序发布给别人，那么此人也成了软件的用户，他也可以出于自己的任何目的运行该软件。然而，你作为软件的发布者，不可将自己的目的强加于你的用户。
+
+“自由地运行程序”意味着没有人可以禁止或阻止用户运行该程序。这和该程序的功能特性没有关系，既不考虑该程序在特定环境下的可用性，也不考虑该程序对特定计算的有效性。
+
+例如，如果代码随意地拒绝某些有意义的输入——或者甚至无条件地失效——这就可能使该程序不那么有用，甚至可能完全无用，但是这并不妨碍用户运行该程序的自由，因此，这和自由之零并无矛盾。如果该程序是自由的，那么其用户可以克服这些无用之处，因为自由之一和自由之三允许用户和社区修改和发表不带有随意代码的改进版本。`;
+/* eslint-enable max-len */
 
 async function basicTest() {
   const doc = await vscode.workspace.openTextDocument();
@@ -195,6 +204,38 @@ async function selectionTest() {
   );
 
   for (let i = 0; i < 100; i++) {
+    backwardSelectWord();
+  }
+
+  assert.strictEqual(
+    editor.selection.isEqual(new vscode.Selection(startPos, startPos)),
+    true,
+  );
+}
+
+async function longTextTest() {
+  const doc = await vscode.workspace.openTextDocument();
+  await vscode.window.showTextDocument(doc);
+  const editor = vscode.window.activeTextEditor;
+  assert.ok(editor !== undefined);
+
+  const startPos = new vscode.Position(0, 0);
+  await editor.edit((edit) => {
+    edit.insert(startPos, longText);
+  });
+  const endPos = editor.document.lineAt(editor.document.lineCount - 1).range.end;
+  editor.selection = new vscode.Selection(startPos, startPos);
+
+  for (let i = 0; i < 300; i++) {
+    forwardSelectWord();
+  }
+
+  assert.strictEqual(
+    editor.selection.isEqual(new vscode.Selection(startPos, endPos)),
+    true,
+  );
+
+  for (let i = 0; i < 300; i++) {
     backwardSelectWord();
   }
 
